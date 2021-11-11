ba64fd86ae843655eefcde179b1c876cdf58a7e1
Diệu Hiền Hồ Đức <162000842@dntu.edu.vn>
2021-11-11 20:56:10 +0700

    do du lieu cho cac thanh dan dung chung

--- ShopMayTinhLamBaoCaoLTWNC_Farmer_18CT111/Controllers/HomeController.cs
+++ ShopMayTinhLamBaoCaoLTWNC_Farmer_18CT111/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Models.Core.Dao;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +13,25 @@ namespace ShopMayTinhLamBaoCaoLTWNC_Farmer_18CT111.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+        // 31 HDDH
+        [ChildActionOnly]
+        public ActionResult Mainmenu()
+        {
+            var model = new MenuDao().ListByGroupId(1);
+            return PartialView(model);
+        }
+        [ChildActionOnly]
+        public ActionResult Topmenu()
+        {
+            var model = new MenuDao().ListByGroupId(2);
+            return PartialView(model);
+        }
+        [ChildActionOnly]
+        public ActionResult Footer()
+        {
+            var model = new FooterDao().GetFooter();
+            return PartialView(model);
         }
     }
 }
