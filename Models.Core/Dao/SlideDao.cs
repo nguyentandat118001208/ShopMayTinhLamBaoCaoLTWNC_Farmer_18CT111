074610b8c57e87296ad40fba3ed7817a6f3e5163
quanglhu2000@gmail.com <quanglhu2000@gmail.com>
2021-11-12 20:31:20 +0700

    do du lieu Binding ra trang chu_32

---
+++ Models.Core/Dao/SlideDao.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models.Core.EF;
+// 32 quang
+namespace Models.Core.Dao
+{
+    public class SlideDao
+    {
+        ShopMayTinhDbContext db = null;
+        public SlideDao()
+        {
+            db = new ShopMayTinhDbContext();
+        }
+        public List<Slide> ListAll()
+        {
+            return db.Slides.Where(x => x.Status == true).OrderBy(y => y.DisplayOrder).ToList();
+        }
+    }
+}
